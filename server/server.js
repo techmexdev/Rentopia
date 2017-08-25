a76291c94268b24382a51fa135b5604a57e01efe
Ben Childs <benjamin.t.childs@gmail.com>
2017-08-25 17:09:12 -0500

    routing config

--- server/server.js
+++ server/server.js
@@ -1,39 +1,46 @@
 const Koa = require('koa')
 const app = new Koa()
-let router = require('koa-router')()
+let api = require('koa-router')()
+let users = require('./routes/users')
+let tenants = require('./routes/tenants')
+let auth = require('./routes/auth')
+let docs = require('./routes/docs')
+let props = require('./routes/props')
+let messages = require('./routes/messages')
+
 
 
 // middleware
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
+// app.use(async (ctx, next) => {
+//   const start = Date.now();
+//   await next();
+//   const ms = Date.now() - start;
+//   ctx.set('X-Response-Time', `${ms}ms`);
+// });
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-});
+// app.use(async (ctx, next) => {
+//   const start = Date.now();
+//   await next();
+//   const ms = Date.now() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+// });
 
-// response
-
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
 // routing
-
+api.use('/users', users.routes())
+api.use('/tenants', tenants.routes())
+api.use('/docs', docs.routes())
+api.use('/props', props.routes())
+api.use('/messages', messages.routes())
+api.use('/auth', auth.routes())
 
 
 
 // use router
 app
-	.use(router.routes())
-	.use(router.allowedMethods())
+	.use(api.routes())
+	.use(api.allowedMethods())
 
 const port = process.env.PORT || 8000
 
