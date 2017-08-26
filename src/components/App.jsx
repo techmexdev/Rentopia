312ad42ddf70009d36913591b1a72a5e9aa9675e
Jordan Hoang <jordan.n.hoang@gmail.com>
2017-08-26 13:33:42 -0500

    Set up webpack config and index js to render components

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import landlord main component
+// import tenant main component
+import Splash from './Splash/splash.jsx'
+
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <Splash />
+  </div>
+)
+
+export default App  
