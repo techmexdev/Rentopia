a299611a93c5ac7c260134b8a4fb8fae16d2eff3
Jordan Hoang <jordan.n.hoang@gmail.com>
2017-08-27 11:22:26 -0500

    created handleSignup function and bound to submit action

--- src/components/Splash/Signup.jsx
+++ src/components/Splash/Signup.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 class Signup extends React.Component {
 
+  handleSignup(e) {
+    e.preventDefault()
+    console.log('hello')
+
+    e.target.firstName.value = ''
+    e.target.lastName.value = ''
+    e.target.email.value = ''
+    e.target.password.value = ''
+  }
+
   render() {
     return (
       <div className="signupForm">
-        <form>
+        <form onSubmit={this.handleSignup.bind(this)}>
           <label>First Name</label><input ref="firstNameInput" name="firstName"></input>
           <label>Last Name</label><input ref="lastNameInput" name="lastName"></input>
           <label>Email Address</label><input ref="emailInput" name="email"></input>
