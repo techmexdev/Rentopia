871207e5fb3501158876ceb70301b4c8630edb09
Baz-Co <shehbaz.sherwani@gmail.com>
2017-08-29 14:16:52 -0500

    Adding postgres user config to db_config

--- db/db_config.js
+++ db/db_config.js
@@ -2,8 +2,12 @@ const { Pool, Client } = require('pg')
 
 // pools will use environment variables
 // for connection information
-const pool = new Pool()
+const pool = new Pool({
+  user: 'postgres',
+  database: 'leasetopia',
+  password: '8BroomH1ll'
+})
 
 module.exports = {
-	query: (text, params) => pool.query(text, params)
+  query: (text, params) => pool.query(text, params)
 }
