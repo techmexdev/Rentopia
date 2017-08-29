d228e11d640dd87399b0804f9d5f8b9f8927b28d
Jordan Hoang <jordan.n.hoang@gmail.com>
2017-08-29 12:12:12 -0500

    Added payment setup component

---
+++ src/components/Payment/PaymentSetup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class PaymentSetup extends React.Component {
+
+  render() {
+    return (
+      <h1>Set up your payments</h1>
+    )
+  }
+}
+
+export default PaymentSetup
