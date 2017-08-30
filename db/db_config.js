c7195b2e0d309c89d7423fae20bf6518c6792af9
Baz-Co <shehbaz.sherwani@gmail.com>
2017-08-30 11:13:16 -0500

    Return 'db_config.js' to shared repo state

---
+++ db/db_config.js
@@ -0,0 +1,9 @@
+const { Pool, Client } = require('pg')
+
+// pools will use environment variables
+// for connection information
+const pool = new Pool()
+   
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+}
