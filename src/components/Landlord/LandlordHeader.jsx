054c2b158b3c0ecb52fbbe24644cb3b553420abf
Eric F <ericdfanning@gmail.com>
2017-08-30 18:46:28 -0500

    set up proper toggle for dropdown menu

--- src/components/Landlord/LandlordHeader.jsx
+++ src/components/Landlord/LandlordHeader.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import React, { Component } from 'react'
+import { Navbar, Nav, NavItem, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-const Header = () => (
-  <Navbar className="navbar" inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand className="brand">
-        <Link to="/">Rentopia</Link>
-      </Navbar.Brand>
-    </Navbar.Header>
-    <Nav>
-      <NavItem>
-        <Link to='/proprietor/properties' className="link">Properties</Link>
-      </NavItem>
-      <NavItem>
-        <Link to='/proprietor/tenants' className="link">Tenants</Link>
-      </NavItem>
-    </Nav>
-    <Nav pullRight>
-      <NavItem>
-        <Link className="logout" to={'/logout'}>Logout</Link>
-      </NavItem>
-    </Nav>
-  </Navbar>
-);
+class Header extends Component {
+    constructor(props) {
+    super() 
+
+    this.state = {
+      dropdownIsOpen: false
+    }
+  }
+
+  toggleDropdown() {
+    this.setState({ dropdownIsOpen: !this.state.dropdownIsOpen }
+  )}
+
+  render() {
+    return (
+      <Navbar className="navbar" inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand className="brand">
+            <Link to="/">Rentopia</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem>
+            <Link to='/proprietor/properties' className="link">Properties</Link>
+          </NavItem>
+          <NavItem>
+            <Link to='/proprietor/tenants' className="link">Tenants</Link>
+          </NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavDropdown title="Profile/Logout" id="nav-dropdown" onToggle={this.toggleDropdown.bind(this)}
+            open={this.state.dropdownIsOpen}>
+            <Link className="dropDownMenu" to="/profile"> Your Profile </Link><br/>
+            <Link className="dropDownMenu" to="/logout"> Logout </Link>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+    )
+  }
+};
 
 export default Header;
